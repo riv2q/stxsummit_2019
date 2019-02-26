2ec3536476173a3cace59be41888197f5b405ff8
Krystian Różycki <rozycki.krystian@gmail.com>
2019-02-26 21:47:51 +0100

    cleaning suff

--- kubernetes/locust/test.py
+++ kubernetes/locust/test.py
@@ -1,28 +1,14 @@
-import random
-import string
-
 from locust import HttpLocust, TaskSet, task
 
 
 class StatlessAppTestSet(TaskSet):
-    """Asset metadata test"""
+    """kubernetes load test"""
 
     @task
     def get_asset_metadata(self):
-        random_text = lambda length: ''.join(
-            random.sample(string.ascii_letters, length))
         url = "/guestbook.php?cmd=set&key=messages&value=,dsa,test"
-
-        with self.client.get(
-            url,
-            catch_response=True
-        ) as response:
-            text_response = str(response.content)
-            if "fail" in text_response:
-                response.failure(text_response)
+        self.client.get(url)
 
 
 class User(HttpLocust):
     task_set = StatlessAppTestSet
-    min_wait = 5000
-    max_wait = 15000
